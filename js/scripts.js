20ad45c103656e3f9efc80f555e19bac67d888f8
Nicholas Castro <10019281@sbschools.org>
2016-03-11 10:45:28 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.scrollspy').scrollSpy();
+$('.slider').slider('pause');
+$('.slider').slider('start');
+$('.slider').slider('next');
+$('.slider').slider('prev');
+  });
+  
